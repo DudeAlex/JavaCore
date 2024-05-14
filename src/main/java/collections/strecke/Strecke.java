d65e635910f9b4ad85479a065f8bb703eab040c6
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2024-05-14 18:29:43 +0200

    add strecke

---
+++ src/main/java/collections/strecke/Strecke.java
@@ -0,0 +1,27 @@
+package collections.strecke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Strecke {
+    int a;
+    int b;
+
+    public Strecke(int a, int b){
+        if(a<b) {
+            this.a = a;
+            this.b = b;
+        } else {
+            this.a = b;
+            this.b = a;
+        }
+    }
+
+
+TestStrecke{
+    @Test
+    public void testStrecke(){
+        Strecke strecke = new Strecke(3,5);
+        assertEquals(3, strecke.a);
+}
