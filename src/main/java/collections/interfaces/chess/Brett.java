25bb3854833929627ff276eb9050ef545e086e7b
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2024-08-17 20:21:55 +0500

    komposition

--- src/main/java/collections/interfaces/chess/Brett.java
+++ src/main/java/collections/interfaces/chess/Brett.java
@@ -2,4 +2,8 @@ package collections.interfaces.chess;
 
 public class Brett {
 
+
+public Brett kombiniere (Brett brett){
+    return null;
+}
 }
--- src/main/java/collections/interfaces/chess/TurmImpl.java
+++ src/main/java/collections/interfaces/chess/TurmImpl.java
@@ -1,6 +1,11 @@
 package collections.interfaces.chess;
 
 public class TurmImpl extends AbstractFigur implements Turm {
+
+    public TurmImpl (int x, int y){
+        super(x, y);
+    }
+
     public Brett gibErlaubteFelder() {
         return null;
         //alle Vertikale und Horizontale Felder
