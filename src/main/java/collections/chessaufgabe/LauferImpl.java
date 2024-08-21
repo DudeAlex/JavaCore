beed09a7720b6d63121486bb1e1d8bc0980f6ece
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2024-08-21 20:47:21 +0500

    newchess new (package chessaufgabe)

--- src/main/java/collections/chessaufgabe/LauferImpl.java
+++ src/main/java/collections/chessaufgabe/LauferImpl.java
@@ -1,26 +1,24 @@
 package collections.chessaufgabe;
 
 public class LauferImpl extends AbstractFigur implements Laufer {
-    public LauferImpl(int x, int y)
-    {
+    public LauferImpl(int x, int y) {
         super(x, y);
     }
 
-    public Brett gibErlaubteFelder()
-    {
+    public Brett gibErlaubteFelder() {
         Brett brett = new Brett();
         //TODO: Erg�nzen Sie den Algorithmus zur Bestimmung der Felder
+        for(int i = 0; i < 8; i++){
+
+        }
         return brett;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         LauferImpl l = new LauferImpl(4,5);
         Brett brett = l.gibErlaubteFelder();
-        for (int j = 1; j <= 8; j++)
-        {
-            for (int i = 1; i <= 8; i++)
-            {
+        for (int j = 1; j <= 8; j++) {
+            for (int i = 1; i <= 8; i++) {
                 if (brett.gibFeld(i, j)) System.out.print("x");
                 else System.out.print("o");
             }
