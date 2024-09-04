975c29cabccf2b1678e08ddd0048b23093f447a8
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2024-09-04 20:55:00 +0500

    streamsio. textfileoutput

---
+++ src/main/java/inputoutputstreams/readerwriterbuffer/ToUpperCaseWriter.java
@@ -0,0 +1,44 @@
+package inputoutputstreams.readerwriterbuffer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class ToUpperCaseWriter {
+    Writer writer;
+    public ToUpperCaseWriter(Writer writer) {
+        this.writer = writer;
+    }
+    public void write(char c) throws IOException {
+        writer.write(Character.toUpperCase(c));
+    }
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for (int i = off; i < off + len; i++)
+            write(cbuf[i]);
+    }
+    public void write(String str, int off, int len) throws IOException {
+        for (int i = off; i < off + len; i++)
+            write(str.charAt(i));
+    }
+
+    public void write(String str) throws IOException{
+        for(int i = 0; i < str.length(); i++)
+            write(str.charAt(i));
+    }
+    //alle (relevanten) Methoden von Writer überschreiben
+    public void flush() throws IOException {
+        writer.flush();
+    }
+    public void close() throws IOException {
+        writer.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileWriter fw = new FileWriter("atouppercasefile.txt");
+        ToUpperCaseWriter tw = new ToUpperCaseWriter(fw);
+        tw.write("Suppe mit dem Löffel löffeln");
+        tw.close();
+    }
+}
+
+
