0b903562e54ae8c7b171aa6f42a0ae328234ced3
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2024-11-11 17:34:03 +0100

    split matrikkelnummer lektion18

---
+++ src/main/java/hausaufgabe/lektion18/studiengangssplitter/studiengangssplitter/MatrikelnummerException.java
@@ -0,0 +1,11 @@
+package hausaufgabe.lektion18.studiengangssplitter.studiengangssplitter;
+
+public class MatrikelnummerException extends RuntimeException{
+    public MatrikelnummerException(){
+
+    }
+
+    public MatrikelnummerException(String message){
+        super(message);
+    }
+}
