85038a5cab77e5d7b819165e320162cba7e6c811
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2024-11-13 19:10:46 +0100

    sms

---
+++ src/main/java/hausaufgabe/lektion20/skat/sms/SmsMain.java
@@ -0,0 +1,11 @@
+package hausaufgabe.lektion20.skat.sms;
+
+public class SmsMain {
+    public static void main(String[] args) {
+
+        Sms sms = new Sms();
+        sms.convertion();
+
+
+    }
+}
