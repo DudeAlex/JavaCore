362af54400c676994a7c78fceb767bae112d06a2
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2024-11-30 17:45:37 +0100

    binarbaum

---
+++ src/main/java/hausaufgabe/lektion21/nährstoffangaben/binarbaum/Binarbaum.java
@@ -0,0 +1,36 @@
+package hausaufgabe.lektion21.nährstoffangaben.binarbaum;
+
+public class Binarbaum {
+
+    Element root;
+
+
+    private class Element {
+
+        String name;
+        Element right;
+        Element left;
+
+        private Element(String name){
+            this.name = name;
+        }
+
+        public void insert(String name){
+            root = addRecursive(root, name);
+        }
+
+
+        private Element addRecursive(Element current, String name){
+            if(current == null) {
+                return new Element(name);
+            }
+            if(current.name.compareTo(name) < 0){
+                current.right = addRecursive(current.right, name);
+            } else if(current.name.compareTo(name) > 0){
+                current.left = addRecursive(current.left, name);
+            }
+            return current;
+        }
+
+    }
+}
