f65dceeed6882af951f5baf1700621eccaa0dc07
DaniyarSabirov <166752732+DaniyarSabirov@users.noreply.github.com>
2025-06-21 18:24:31 +0200

    iterator and iterable

---
+++ src/main/java/collections/viertesemester/iterator/RouletteZahlenMain.java
@@ -0,0 +1,16 @@
+package collections.viertesemester.iterator;
+
+public class RouletteZahlenMain {
+    public static void main(String[] args) {
+
+
+
+        RouletteZahlen rouletteZahlen = new RouletteZahlen();
+
+        for(Integer i : rouletteZahlen){
+            System.out.println(i);
+        }
+
+
+    }
+}
